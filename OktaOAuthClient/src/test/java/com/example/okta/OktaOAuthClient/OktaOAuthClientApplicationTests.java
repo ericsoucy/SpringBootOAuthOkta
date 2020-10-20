1e5c3f3cc3d0d9d7e1586044475c4dbecc13feca
eric soucy <eric.soucy@gmail.com>
2020-10-19 20:29:45 -0400

    okta

---
+++ OktaOAuthClient/src/test/java/com/example/okta/OktaOAuthClient/OktaOAuthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.okta.OktaOAuthClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktaOAuthClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
